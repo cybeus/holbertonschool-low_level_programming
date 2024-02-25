5a1e19dc2e55d4583fe788255b55ca8fd5a57b99
cybeus <zaurquluyevv@gmail.com>
2024-02-25 19:23:27 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char alhabet = 'a';
+
+	while (int i = 0 < 10)
+	{
+		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+	_putchar('\n');
+	i++;
+	}
+}

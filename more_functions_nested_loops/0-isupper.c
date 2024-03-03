26e888eadcb2bbfc15db4a6bc3e0c229f14cbbbb
cybeus <zaurquluyevv@gmail.com>
2024-03-03 17:37:31 +0400

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * @c:var
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (c < 90 && c > 65)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
